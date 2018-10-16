2c1caf66447ab7aca428adc566b960d8942d3a6b
Emir Korkmaz <emir.korkmaz@payrov.com.tr>
2018-10-16 21:05:35 +0300

    2

--- src/main/java/ListSubSet.java
+++ src/main/java/ListSubSet.java
@@ -3,30 +3,32 @@ import java.util.*;
 public class ListSubSet {
 
     static <T> List<List<T>> getSubsets(List<T> values, int teamSize) {
-        T[] elements = (T[]) values.toArray();
-        List<List<T>> powerSet = new ArrayList<>();
+        T[] valuesArray = (T[]) values.toArray();
+        List<List<T>> allSubsets = new ArrayList<>();
 
-        final int elementArrayLength = 1 << elements.length;
-        for (int binarySet = 0; binarySet < elementArrayLength; binarySet++) {
+        //total number of subsets of given list equals to 2^n while n is number of elements in list
+        final int valuesArrayLength = 1 << valuesArray.length;
+        for (int setIndex = 0; setIndex < valuesArrayLength; setIndex++) {
             List<T> subset = new ArrayList<>();
-            for (int bit = 0; bit < elements.length; bit++) {
+            for (int bit = 0; bit < valuesArray.length; bit++) {
                 int mask = 1 << bit;
-                if ((binarySet & mask) != 0) {
-                    subset.add(elements[bit]);
+                if ((setIndex & mask) != 0) {
+                    subset.add(valuesArray[bit]);
                 }
             }
 
             if (subset.size() == teamSize)
-                powerSet.add(subset);
+                allSubsets.add(subset);
         }
 
-        return powerSet;
+        return allSubsets;
     }
 
     public static void main(String[] args) {
         Integer[] arr = {5, 10, 4, 8, 3, 6, 5, 10, 4, 8, 3, 6};
         List<Integer> values = Arrays.asList(arr);
         int teamSize = 4;
+        int numberOfTeams = arr.length / teamSize;
 
         List<List<Integer>> result = getSubsets(values, teamSize);
 
@@ -43,8 +45,8 @@ public class ListSubSet {
 
         resultList.sort(Comparator.naturalOrder());
 
-        int finalResult = -1;
-        for(int i = 0; i < teamSize; i++) {
+        int finalResult = 0;
+        for(int i = 0; i < numberOfTeams; i++) {
             finalResult += resultList.get(i);
         }
 
